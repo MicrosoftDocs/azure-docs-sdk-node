41f9c8fed13c7f40279b3a79add8703b086f83b4
Simon Wu <siw@microsoft.com>
2017-06-09 17:03:12 +0800

    Create OnSuccess.cmd

--- scripts/OnSuccess.cmd
+++ scripts/OnSuccess.cmd
@@ -1,2 +1,2 @@
 git add .
-git diff --quiet --exit-code --cached || git commit -m "Update Reference Content" && git push -u origin master && echo "Document updated"
+git diff --quiet --exit-code --cached || git commit -m "Update Reference Content" && git push -u origin opendev && echo "Document updated"

7287cadfd3903d8e519e8ec57fe9515f96a9eb4d
Yannan Wang <15991684027@163.com>
2017-06-13 09:50:57 +0800

    Create build.js

--- scripts/build.js
+++ scripts/build.js
@@ -12,6 +12,8 @@ var dest = 'docs-ref-autogen';
 var doc = 'Documentation';
 var configPath = 'node2docfx.json';
 var tempConfigPath = '_node2docfx_temp.json';
+var filenameMaxLength = 100;
+var packagesToFilter = ['azure-arm-datalake-store'];
 
 function itemsByType(type) {
   return packageNames.filter(function (value) {
@@ -75,7 +77,28 @@ fs.unlink(tempConfigPath);
 fse.copySync(path.join(src, doc), path.join(dest, doc));
 console.log('Finish copying documentation');
 
-// 5. generate root toc
+// 5. remove files with too long filename that breaks DocFX
+packagesToFilter.forEach(function (p) {
+  var uidsToFilter = [];
+  fs.readdirSync(path.join(dest, p)).forEach(function (f) {
+    if (f.length > filenameMaxLength) {
+      var filePath = path.join(dest, p, f);
+      uidsToFilter.push(yaml.safeLoad(fs.readFileSync(filePath)).items[0].uid);
+      fs.unlinkSync(filePath);
+      console.log('File with too long name is removed from ref folder: ' + filePath);
+    }
+  });
+  var tocPath = path.join(dest, p, 'toc.yml');
+  if (fs.existsSync(tocPath)) {
+    var toc = yaml.safeLoad(fs.readFileSync(tocPath)).filter(function(i){
+      return uidsToFilter.indexOf(i.uid) == -1;
+    });
+    fs.writeFileSync(tocPath, yaml.safeDump(toc));
+    console.log('TOC updated after removing files: ' + tocPath);
+  }
+});
+
+// 6. generate root toc
 var rootToc = [];
 var rootTocPath = path.join(dest, 'toc.yml');
 var subTocs = glob.sync(path.join(dest, '**/toc.yml'));
